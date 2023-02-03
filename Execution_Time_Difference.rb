aaf12b71ee34486524fbdc116be25c4705974c4d
kevalshah598 <kevalshah598@gmail.com>
2023-02-03 14:24:44 -0500

    my_min part 1

--- Execution_Time_Difference.rb
+++ Execution_Time_Difference.rb
@@ -1,38 +1,27 @@
-# def my_min(array)
-#     # min = array[-1]
-#     # array.each_with_index do |num1, i|
-#     #     min = num1 if num1 < min
-#     #     array[i + 1..-1].each_with_index do |num2, j|
-#     #         min = num2 if num2 < num1
-#     #     end
-#     # end
-#     # return min
-
-#     min = 0
-#     i = 0
-
-#     while i < array.length
-#         j = i + 1
-#         while j < array.length
-#             min = array[i] if array[j] > array[i]
-#             j += 1
-#         end
-        
-#         i += 1
-#     end
-
-#     return min
-# end
-
+require 'byebug'
 def my_min(array)
     min = array[0]
-
-    array.each do |num|
-        min = num if num < min
+    # new_min = array[0]
+    array.each_with_index do |num1, i|
+        debugger
+        array[i + 1..-1].each_with_index do |num2, j|
+            new_min = min 
+            new_min = num2 if num2 < new_min
+            min = new_min if new_min < min
+        end
     end
-
-    min
+    return min
 end
+
+# def my_min(array)
+#     min = array[0]
+
+#     array.each do |num|
+#         min = num if num < min
+#     end
+
+#     min
+# end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list) # => -5
